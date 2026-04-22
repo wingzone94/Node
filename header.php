f000992ebf50a8340fbf7a10d5629193d6e6f5fa
Tatsuya Saito <stardrive37@gmail.com>
2026-04-22 20:17:03 +0900

    feat: release version 0.2.0 with Luminouscore theme colors and favicon

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/Luminouscore.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <?php wp_head(); ?>
